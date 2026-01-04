9333ed1d272a5f4c3ed21ffbff5d17de29e9f996
Juan122113 <ammiel89@hotmail.com>
2026-01-04 08:45:39 -0300

    feat: add unit selection logic and header styling
    • Implement toggle functionality for the units dropdown using JavaScript. • Structure unit options using <optgroup> for Temperature, Wind Speed, and Precipitation. • Refactor header layout with Flexbox for improved alignment. • Update image alt text and clean up internal CSS from index.html.

---
+++ main.js
@@ -0,0 +1,6 @@
+const unitsContainer = document.getElementById("units-container");
+const unitsSelect = document.getElementById("units-select");
+
+unitsContainer.addEventListener('click', function() {
+    unitsSelect.style.display = "block";
+})
